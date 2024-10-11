1bd96cd61cf0670879d0be2da4eac6bc7e561719
jack-345 <jou@zagmail.gonzaga.edu>
2024-10-11 08:21:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,19 @@ const addTaskButton = document.getElementById("add-task");
 //load tasks from local
 const tasks = loadTasks();
 
+tasks.forEach(element => {
+    addTask(task, taskList);
+});
+
 //add event listener for new task
-addTaskButton.addEventListener("click" () => {
+addTaskButton.addEventListener("click", () => {
     const task = newTaskInput.value.trim();
-    tasks.push(task);
-    saveTasks(tasks);
-    clearInput(newTaskInput);
+
+    if (task) {
+        addTask(task, taskList);
+        tasks.push(task);
+        saveTasks(tasks);
+        clearInput(newTaskInput);
+    }
 } );
 //
